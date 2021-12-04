b1213e838d45446c37e18b4d41e5d5d7b9115b0b
SidorovDaniil <svdenielvs@gmail.com>
2021-12-04 16:15:25 +0300

    new h1.5

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -8,18 +8,18 @@ int main(){
     std::ifstream fin;
     fin.open(path);
 
-    if (fin.is_open()) {
-        double str;
-
-        while (!fin.eof()) {
-
-            fin >> str;
-            std::cout << str << std::endl;
-        }
-    }
-    else{
-        std::cout<< "No file";
-    }
+//    if (fin.is_open()) {
+//        double str;
+//
+//        while (!fin.eof()) {
+//
+//            fin >> str;
+//            std::cout << str << std::endl;
+//        }
+//    }
+//    else{
+//        std::cout<< "No file";
+//    }
     std::cout << "Leftmost: "<< 0 << " " << 0 <<std::endl;
     std::cout << "Rightmost: " << 0 << " " << 0 <<std::endl;
 
